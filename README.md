c78c7f84a48658ad61b41756d2d0c3de642f67f8
Suraj N <surajn@mactores.com>
2022-03-07 18:39:48 +0530

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # bitglass-mysql-maxscale-setup
 bitglass-mysql-maxscale-setup
+
+
+Todo: Add all the files to root
+Todo: Add packages
+Todo: Define all functions
+Todo: Define logging
+Todo: Recheck try catch blocks
+Todo: Add argparser
+Todo: Add configparser
---
+++ main.py
@@ -0,0 +1,19 @@
+
+import logging
+
+
+def main():
+    #Create all objects
+
+
+    #Connection to mysql slave
+    logging.info("Establishing connection to mysql")
+
+    #If connection has an error, switch the maxscale master
+    logging.info("Switching maxscale")
+
+
+if __name__ == "__main__":
+    main()
+
+
